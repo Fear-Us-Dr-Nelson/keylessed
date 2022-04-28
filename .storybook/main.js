e1da446e1a7ed2d30acade5d753f863fd7b97e70
Reetik Patel <Reetik2000@yahoo.com>
2022-04-28 01:48:38 -0500

    🎉 Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  },
+  staticDirs: ['../public'],
+  webpackFinal: async (config, { configType }) => {
+    const rules = config.module.rules;
+    const fileLoaderRule = rules.find(rule => rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  const config = {
+    ...defaultConfig,
+    reactStrictMode: true,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      });
+  
+      return config;
+    }
+  }
+
+  return config;
+}
